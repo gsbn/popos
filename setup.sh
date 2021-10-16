9e2f46fba805fd686f2736720412e618dfda6fe2
gsbn <gsbn@users.noreply.github.com>
2021-10-16 13:23:47 +0100

    Bash shell update working

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # Fonts Install
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip -O ~/MesloInstall.zip
-unzip -o ~/MesloInstall.zip -d ~/MesloInstall
+echo "Downloading fonts..."
+wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip -O ~/MesloInstall.zip
+echo "Copying fonts..."
+unzip -o -q ~/MesloInstall.zip -d ~/MesloInstall
 rm ~/MesloInstall.zip
 font_dir="$HOME/.local/share/fonts/Meslo/TrueType"
 mkdir -p $font_dir
@@ -9,12 +11,21 @@ rm -r ~/MesloInstall
 fc-cache -f $font_dir
 
 # Oh My Posh - Basic Install
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo wget -q --show-progress https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 
 # Oh My Posh - Theme Install
-mkdir ~/.poshthemes
-wget https://raw.githubusercontent.com/gsbn/popos/main/poshthemes/myterminal.omp.json -O ~/.poshthemes/myterminal.omp.json
+mkdir -p ~/.poshthemes
+wget -q --show-progress https://raw.githubusercontent.com/gsbn/popos/main/poshthemes/myterminal.omp.json -O ~/.poshthemes/myterminal.omp.json
 
 # Update Bash shell
+update_file="$HOME/.bashrc"
+bash_header="#OhMyPosh-Config"
+bash_footer="#OhMyPosh-Config-End"
+bash_text='eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/myterminal.omp.json)"'
+bash_full="$bash_header\n$bash_text\n$bash_footer"
+
+sed -i "/$bash_header/,/$bash_footer/d" $update_file
+echo -e $bash_full >> $update_file
+
 source ~/.bashrc
